1921dd4dbbc5fd2ba03e203326353b1af49d6c50
Marcelle Queiroz <marcellerq27@gmail.com>
2026-08-27 00:34:34 -0300

    add projects

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Portfolio carregado!");
+const projectsGrid = document.querySelector(".projects-grid");
+
+if (projectsGrid) {
+	const projectCards = [...projectsGrid.children];
+
+	projectCards.forEach((projectCard) => {
+		const duplicatedCard = projectCard.cloneNode(true);
+
+		duplicatedCard.setAttribute("aria-hidden", "true");
+		projectsGrid.appendChild(duplicatedCard);
+	});
+}
